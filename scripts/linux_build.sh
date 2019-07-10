c67a3edb605870aef1514625c0624ae4b34d00b6
Oleksandr Rudenko <ext-oleksandr.rudenko@here.com>
2019-07-10 17:34:45 +0300

    Tavis CI setup without docker Using xenial dist Reduce dependencies Using packages:            - cmake            - libssl-dev            - libcurl4-openssl-dev            - libboost-all-dev Resolves: OLPEDGE-420
    Signed-off-by: Oleksandr Rudenko <ext-oleksandr.rudenko@here.com>

--- scripts/linux_build.sh
+++ scripts/linux_build.sh
@@ -1,15 +1,10 @@
 #!/bin/bash -xe
-cd mnt/test
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -EDGE_SDK_BUILD_EXAMPLES=ON ..
 make -j8
 cd ..
 CPP_TEST_SOURCE_AUTHENTICATION=build/olp-cpp-sdk-authentication/test
 CPP_TEST_SOURCE_CORE=build/olp-cpp-sdk-core/tests
-CPP_O="$(find build/* | grep cpp.o)"
-tar -czf artifacts.tar.gz --exclude='*.git' \
-    "build/lib" \
-    `find build -type f -executable` \
 echo ">>> Authentication Test ... >>>"
 $CPP_TEST_SOURCE_AUTHENTICATION/unit/olp-authentication-test --gtest_output="xml:report.xml" --gtest_filter="AuthenticationOfflineTest.*"
 echo ">>> Core - Cache Test ... >>>"
