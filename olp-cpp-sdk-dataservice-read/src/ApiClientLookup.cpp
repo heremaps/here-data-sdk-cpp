a438a9b12c0c330d820637c4b60165eac76e70bc
Sergii Vostrikov <ext-sergii.vostrikov@here.com>
2019-07-22 14:10:35 +0300

    ApiClientLookup class now logs all its major steps
    Relates-to: OLPEDGE-547
    
    Signed-off-by: Sergii Vostrikov <ext-sergii.vostrikov@here.com>

--- olp-cpp-sdk-dataservice-read/src/ApiClientLookup.cpp
+++ olp-cpp-sdk-dataservice-read/src/ApiClientLookup.cpp
@@ -22,6 +22,8 @@
 #include <olp/core/client/ApiError.h>
 #include <olp/core/client/HRN.h>
 #include <olp/core/client/OlpClient.h>
+#include <olp/core/logging/Log.h>
+
 #include "generated/api/PlatformApi.h"
 #include "generated/api/ResourcesApi.h"
 
@@ -30,6 +32,9 @@ namespace dataservice {
 namespace read {
 using namespace olp::client;
 
+namespace {
+constexpr auto kLogTag = "ApiClientLookup";
+
 const std::map<std::string, std::string>& getDatastoreServerUrl() {
   static std::map<std::string, std::string> sDatastoreServerUrl = {
       {"here-dev", "data.api.platform.in.here.com"},
@@ -37,16 +42,22 @@ const std::map<std::string, std::string>& getDatastoreServerUrl() {
 
   return sDatastoreServerUrl;
 }
+}  // namespace
 
 CancellationToken ApiClientLookup::LookupApi(std::shared_ptr<OlpClient> client,
                                              const std::string& service,
                                              const std::string& service_version,
                                              const HRN& hrn,
                                              const ApisCallback& callback) {
+  LOG_TRACE_F(kLogTag, "LookupApi(%s/%s): %s", service.c_str(),
+              service_version.c_str(), hrn.partition.c_str());
   // compare the hrn
   const auto& datastoreServerUrl = getDatastoreServerUrl();
   auto findResult = datastoreServerUrl.find(hrn.partition);
+
   if (findResult == datastoreServerUrl.cend()) {
+    LOG_INFO_F(kLogTag, "LookupApi(%s/%s): %s could not find HRN",
+               service.c_str(), service_version.c_str(), hrn.partition.c_str());
     callback(ApiError(ErrorCode::NotFound,
                       std::string()));  // TODO better error description?
     return CancellationToken();
@@ -56,6 +67,9 @@ CancellationToken ApiClientLookup::LookupApi(std::shared_ptr<OlpClient> client,
 
     if (service == "config") {
       // scan apis at platform apis
+      LOG_INFO_F(kLogTag, "LookupApi(%s/%s): %s - config service",
+                 service.c_str(), service_version.c_str(),
+                 hrn.partition.c_str());
       return PlatformApi::GetApis(
           client, service, service_version,
           [callback](PlatformApi::ApisResponse response) {
@@ -63,6 +77,9 @@ CancellationToken ApiClientLookup::LookupApi(std::shared_ptr<OlpClient> client,
           });
     } else {
       // scan apis at resource endpoint
+      LOG_INFO_F(kLogTag, "LookupApi(%s/%s): %s - resource service",
+                 service.c_str(), service_version.c_str(),
+                 hrn.partition.c_str());
       return ResourcesApi::GetApis(
           client, hrn.ToCatalogHRNString(), service, service_version,
           [callback](PlatformApi::ApisResponse response) {
@@ -76,17 +93,29 @@ CancellationToken ApiClientLookup::LookupApiClient(
     std::shared_ptr<OlpClient> client, const std::string& service,
     const std::string& service_version, const HRN& hrn,
     const ApiClientCallback& callback) {
+  LOG_TRACE_F(kLogTag, "LookupApiClient(%s/%s): %s", service.c_str(),
+              service_version.c_str(), hrn.partition.c_str());
   return ApiClientLookup::LookupApi(
       client, service, service_version, hrn,
 
-      [client, callback](ApisResponse response) {
+      [client, callback, service, service_version, hrn](ApisResponse response) {
         if (!response.IsSuccessful()) {
+          LOG_INFO_F(
+              kLogTag, "LookupApiClient(%s/%s): %s - unsuccessful: %s",
+              service.c_str(), service_version.c_str(), hrn.partition.c_str(),
+              response.GetError().GetMessage().c_str());
           callback(response.GetError());
         } else if (response.GetResult().size() < 1) {
+          LOG_INFO_F(
+              kLogTag, "LookupApiClient(%s/%s): %s - service not available",
+              service.c_str(), service_version.c_str(), hrn.partition.c_str());
           // TODO use defined ErrorCode
           callback(ApiError(ErrorCode::ServiceUnavailable,
                             "Service/Version not available for given HRN"));
         } else {
+          LOG_INFO_F(kLogTag, "LookupApiClient(%s/%s): %s - OK",
+                     service.c_str(), service_version.c_str(),
+                     hrn.partition.c_str());
           client->SetBaseUrl(response.GetResult().at(0).GetBaseUrl());
           callback(*(client.get()));
         }
