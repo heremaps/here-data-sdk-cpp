59641de816b1de6c8372889bae4c6968ca4b3979
Yaroslav Stefinko <ystefinko@intellias.com>
2019-08-05 14:53:28 +0300

    Fix1.
    Resolves: OLPEDGE-568
    Signed-off-by: Yaroslav Stefinko <ystefinko@intellias.com>

--- scripts/android/build.sh
+++ scripts/android/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mv android-5.0 android-5.0_source && mkdir -p android-5.0/platforms/android-21 && cp -r android-5.0_source/* android-5.0/platforms/android-21/ && rm -rf android-5.0_source
+mv android-9 android-9_source && mkdir -p android-9/platforms/android-28 && cp -r android-9_source/* android-9/platforms/android-28/ && rm -rf android-9_source
 mkdir -p build && cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE="$WORKSPACE/android-ndk-r19c/build/cmake/android.toolchain.cmake" -DANDROID_ABI=arm64-v8a -DEDGE_SDK_ENABLE_TESTING=NO
+cmake .. -DCMAKE_TOOLCHAIN_FILE="$WORKSPACE/android-ndk-r20/build/cmake/android.toolchain.cmake"  -DANDROID_ABI=arm64-v8a -DEDGE_SDK_ENABLE_TESTING=NO -DEDGE_SDK_BUILD_EXAMPLES=ON
 make
