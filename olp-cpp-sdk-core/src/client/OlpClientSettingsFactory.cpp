4092d6fe58ffa7783c1e8c92c9380c04526fff40
Sergii Vostrikov <ext-sergii.vostrikov@here.com>
2019-08-06 17:11:51 +0300

    TaskScheduler added to OlpClientSettings
    In order to setup OlpClientSettings user should fill-in task_scheduler
    field. One of the ways to do that is so use newly introduced method
    of OlpClientSettingsFactory class.
    
    Resolves: OLPEDGE-453
    
    Signed-off-by: Sergii Vostrikov <ext-sergii.vostrikov@here.com>

---
+++ olp-cpp-sdk-core/src/client/OlpClientSettingsFactory.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+#include "olp/core/client/OlpClientSettingsFactory.h"
+
+#include <olp/core/porting/make_unique.h>
+#include <olp/core/thread/TaskScheduler.h>
+#include <olp/core/thread/ThreadPoolTaskScheduler.h>
+
+namespace olp {
+namespace client {
+
+std::unique_ptr<thread::TaskScheduler>
+OlpClientSettingsFactory::CreateDefaultTaskScheduler(size_t thread_count) {
+  return std::make_unique<thread::ThreadPoolTaskScheduler>(thread_count);
+}
+
+}  // namespace client
+}  // namespace olp
