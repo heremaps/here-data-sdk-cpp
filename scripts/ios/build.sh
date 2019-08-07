9f749a0591c893d06f6c64d9b0a4adf7c20896ac
olrudenk <ext-oleksandr.rudenko@here.com>
2019-08-07 12:35:40 +0300

    Setup Travis CI to build Android and IOS EXAMPLES Added flags EDGE_SDK_BUILD_EXAMPLES=ON to Cmake Licenses accepted Resolved Boost dependencies Adedd OSX image xcode10.2
    Resolves: OLPEDGE-568
    Signed-off-by: Oleksandr Rudenko <ext-oleksandr.rudenko@here.com>

--- scripts/ios/build.sh
+++ scripts/ios/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p build && cd build
-cmake ../ -GXcode -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/iOS.cmake -DPLATFORM=iphoneos -DEDGE_SDK_ENABLE_TESTING=NO -DSIMULATOR=YES
+cmake ../ -GXcode -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/iOS.cmake -DPLATFORM=iphoneos -DEDGE_SDK_ENABLE_TESTING=NO -DSIMULATOR=YES -DEDGE_SDK_BUILD_EXAMPLES=ON
 xcodebuild
 cd ..
