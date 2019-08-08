bdde580d7dd823e2793f576d115146372947f37d
Sergii Vostrikov <ext-sergii.vostrikov@here.com>
2019-08-08 15:30:02 +0300

    Network member added to OlpClientSettings
    This field meant to be used by all client classes to access web
    services.
    
    Resolves: OLPEDGE-542
    
    Signed-off-by: Sergii Vostrikov <ext-sergii.vostrikov@here.com>

--- olp-cpp-sdk-core/src/client/OlpClientSettingsFactory.cpp
+++ olp-cpp-sdk-core/src/client/OlpClientSettingsFactory.cpp
@@ -19,9 +19,11 @@
 
 #include "olp/core/client/OlpClientSettingsFactory.h"
 
-#include <olp/core/porting/make_unique.h>
-#include <olp/core/thread/TaskScheduler.h>
-#include <olp/core/thread/ThreadPoolTaskScheduler.h>
+#include "olp/core/porting/make_unique.h"
+#include "olp/core/thread/TaskScheduler.h"
+#include "olp/core/thread/ThreadPoolTaskScheduler.h"
+
+#include "olp/core/http/Network.h"
 
 namespace olp {
 namespace client {
@@ -29,6 +31,11 @@ namespace client {
 std::unique_ptr<thread::TaskScheduler>
 OlpClientSettingsFactory::CreateDefaultTaskScheduler(size_t thread_count) {
   return std::make_unique<thread::ThreadPoolTaskScheduler>(thread_count);
+}
+
+std::unique_ptr<http::Network>
+OlpClientSettingsFactory::CreateDefaultNetworkRequestHandler() {
+  return nullptr;
 }
 
 }  // namespace client
