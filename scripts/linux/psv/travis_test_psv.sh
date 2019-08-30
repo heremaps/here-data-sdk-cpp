d66671f436c0b6d842a94d870514be0ca47d2269
Andrei Popescu <andrei.popescu@here.com>
2019-08-30 11:51:32 +0200

    Fixes the cache clear key for partitions and the according test.
    When adding a layers partition metadata response to the cache the
    key is hrn::layer::version::partitions. In case we receive a 403 error
    we try to delete the entire entry from the cache with RemoveKeysWithPrefix()
    were the key was hrn::layer::::partitions which didn't work as the entries
    include version also. Changing the key in case no version is provided to
    hrn::layer:: which will get successfull prefix comparison in the cache impl.
    
    Relates-to: OLPEDGE-643
    
    Signed-off-by: Andrei Popescu <andrei.popescu@here.com>

--- scripts/linux/psv/travis_test_psv.sh
+++ scripts/linux/psv/travis_test_psv.sh
@@ -16,7 +16,7 @@ $CPP_TEST_SOURCE_CORE/network/olp-core-network-test --gtest_output="xml:report4.
 echo ">>> Core - Thread Test ... >>>"
 $CPP_TEST_SOURCE_CORE/thread/thread-test --gtest_output="xml:report5.xml"
 echo ">>> Dataservice read Test ... >>>"
-$CPP_TEST_SOURCE_DARASERVICE_READ/unit/olp-dataservice-read-test --gtest_output="xml:report6.xml" --gtest_filter=-"TestOnline/*":"*GetPartitions403CacheClear*"
+$CPP_TEST_SOURCE_DARASERVICE_READ/unit/olp-dataservice-read-test --gtest_output="xml:report6.xml" --gtest_filter=-"TestOnline/*"
 echo ">>> Dataservice write Test ... >>>"
 $CPP_TEST_SOURCE_DARASERVICE_WRITE/olp-dataservice-write-test --gtest_output="xml:report7.xml" --gtest_filter=-"*Online*":"TestCacheMock*"
 bash <(curl -s https://codecov.io/bash)
