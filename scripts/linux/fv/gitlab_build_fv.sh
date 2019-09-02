7102a420a1aa43ec42cc18782fcf039dbcd332a4
stefinko <ext-yaroslav.stefinko@here.com>
2019-09-02 17:41:28 +0300

    Shared libs tuned ON. Linux libsegfault library added for segmentation fault crash identification in logs.
    Signed-off-by: stefinko <ext-yaroslav.stefinko@here.com>

--- scripts/linux/fv/gitlab_build_fv.sh
+++ scripts/linux/fv/gitlab_build_fv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -EDGE_SDK_BUILD_EXAMPLES=ON -DEDGE_SDK_BUILD_DOC=ON ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -EDGE_SDK_BUILD_EXAMPLES=ON -DEDGE_SDK_BUILD_DOC=ON -DBUILD_SHARED_LIBS=ON ..
 make -j8
 make docs
 cd ..
--- scripts/linux/psv/travis_build_psv.sh
+++ scripts/linux/psv/travis_build_psv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -EDGE_SDK_BUILD_EXAMPLES=ON ..
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -EDGE_SDK_BUILD_EXAMPLES=ON -DBUILD_SHARED_LIBS=ON ..
 make -j8
 cd ..
