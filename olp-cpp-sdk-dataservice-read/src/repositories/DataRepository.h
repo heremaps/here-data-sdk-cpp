2415ec6006ae7d85967fb73ea358ff8fb64b2eb2
Sergii Vostrikov <ext-sergii.vostrikov@here.com>
2019-09-03 15:08:36 +0200

    VersionedLayerClient class implementation
    The class' GetData method is introduced.
    It fetches data from a layer using a partition id or data handle.
    The data is fetched asynchronously if the task scheduler is set
    properly. The synchronous implementation of the logic is placed to
    DataRepository::GetData() as it fits better from an architectural
    point of view.
    
    Resolves: OLPEDGE-759
    
    Signed-off-by: Sergii Vostrikov <ext-sergii.vostrikov@here.com>

--- olp-cpp-sdk-dataservice-read/src/repositories/DataRepository.h
+++ olp-cpp-sdk-dataservice-read/src/repositories/DataRepository.h
@@ -54,6 +54,11 @@ class DataRepository final {
                const std::string& layerType, const read::DataRequest& request,
                const read::DataResponseCallback& callback);
 
+  static DataResponse GetVersionedData(
+      olp::client::HRN catalog, std::string layer_id,
+      olp::client::OlpClientSettings client_settings, DataRequest data_request,
+      olp::client::CancellationContext context);
+
   static DataResponse GetBlobData(
       const client::HRN& catalog, const std::string& layer,
       const std::string& service, const DataRequest& data_request,
