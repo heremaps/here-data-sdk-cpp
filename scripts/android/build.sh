8af4e7809feda1f7e485ee15d6f200939e6bc16a
Serhii Lysenko <ext-serhii.lysenko@here.com>
2019-09-03 19:09:34 +0300

    Add ccache support to travis-ci builds for linux
    Relates-To: OLPEDGE-689
    Signed-off-by: Serhii Lysenko <ext-serhii.lysenko@here.com>

--- scripts/android/build.sh
+++ scripts/android/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -e
 mv android-9 android-9_source && mkdir -p android-9/platforms/android-28 && cp -r android-9_source/* android-9/platforms/android-28/ && rm -rf android-9_source
 mkdir -p build && cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE="$WORKSPACE/android-ndk-r20/build/cmake/android.toolchain.cmake" -DANDROID_PLATFORM=android-28 -DANDROID_ABI=arm64-v8a -DEDGE_SDK_ENABLE_TESTING=NO -DEDGE_SDK_BUILD_EXAMPLES=ON
+cmake .. -DCMAKE_TOOLCHAIN_FILE="$WORKSPACE/android-ndk-r20/build/cmake/android.toolchain.cmake" \
+	-DANDROID_PLATFORM=android-28 \
+	-DANDROID_ABI=arm64-v8a \
+	-DEDGE_SDK_ENABLE_TESTING=NO \
+	-DEDGE_SDK_BUILD_EXAMPLES=ON 
+
 #cmake --build .
 sudo make install -j16
 cd $WORKSPACE/build/examples/dataservice-read/android
