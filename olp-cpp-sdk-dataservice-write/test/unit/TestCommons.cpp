56f4beb80d3d436e92a6d5c37e0ecf91e34d93a1
Serhii Lysenko <ext-serhii.lysenko@here.com>
2019-09-09 13:28:07 +0300

    Enable Index Layer Client write tests
     * fix disabled tests
     * refactor cancellation tests
     * put common code into TestCommons.{h|cpp}
    
    Relates-to: OLPEDGE-710
    
    Signed-off-by: Serhii Lysenko <ext-serhii.lysenko@here.com>

---
+++ olp-cpp-sdk-dataservice-write/test/unit/TestCommons.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+#include "TestCommons.h"
+
+#include <cstring>
+#include <ostream>
+#include <thread>
+
+using namespace olp::client;
+
+std::tuple<olp::http::RequestId, NetworkCallback, CancelCallback>
+generateNetworkMocks(std::shared_ptr<std::promise<void>> pre_signal,
+                     std::shared_ptr<std::promise<void>> wait_for_signal,
+                     MockedResponseInformation response_information,
+                     std::shared_ptr<std::promise<void>> post_signal) {
+  using namespace olp::http;
+
+  static std::atomic<RequestId> s_request_id{
+      static_cast<RequestId>(RequestIdConstants::RequestIdMin)};
+
+  olp::http::RequestId request_id = s_request_id.fetch_add(1);
+
+  auto completed = std::make_shared<std::atomic_bool>(false);
+
+  // callback is generated when the send method is executed, in order to receive
+  // the cancel callback, we need to pass it to store it somewhere and share
+  // with cancel mock.
+  auto callback_placeholder = std::make_shared<olp::http::Network::Callback>();
+
+  auto mocked_send =
+      [request_id, completed, pre_signal, wait_for_signal, response_information,
+       post_signal, callback_placeholder](
+          NetworkRequest request, Network::Payload payload,
+          Network::Callback callback, Network::HeaderCallback,
+          Network::DataCallback data_callback) -> olp::http::SendOutcome {
+    *callback_placeholder = callback;
+
+    auto mocked_network_block = [request, pre_signal, wait_for_signal,
+                                 completed, callback, response_information,
+                                 post_signal, payload]() {
+      // emulate a small response delay
+      std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+      // notify waiting thread that we reached the network code
+      pre_signal->set_value();
+
+      // wait until test cancel request during execution
+      wait_for_signal->get_future().get();
+
+      // in the case request was not canceled return the expected payload
+      if (!completed->exchange(true)) {
+        const auto data_len = std::strlen(response_information.data);
+        payload->write(response_information.data, data_len);
+        callback(NetworkResponse().WithStatus(response_information.status));
+      }
+
+      // notify that request finished
+      post_signal->set_value();
+    };
+
+    // simulate that network code is actually running in the background.
+    std::thread(std::move(mocked_network_block)).detach();
+
+    return SendOutcome(request_id);
+  };
+
+  auto mocked_cancel = [completed,
+                        callback_placeholder](olp::http::RequestId id) {
+    if (!completed->exchange(true)) {
+      auto cancel_code = static_cast<int>(ErrorCode::CANCELLED_ERROR);
+      (*callback_placeholder)(NetworkResponse()
+                                  .WithError("Cancelled")
+                                  .WithCancelled(true)
+                                  .WithStatus(cancel_code));
+    }
+  };
+
+  return std::make_tuple(request_id, std::move(mocked_send),
+                         std::move(mocked_cancel));
+}
