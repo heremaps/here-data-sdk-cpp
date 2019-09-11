ae4ee5ccb8835838e782bdf6b1989d613a0141b6
Serhii Lysenko <ext-serhii.lysenko@here.com>
2019-09-11 18:36:45 +0300

    Adapt authentication tests to Pitch-Fork form
     * fixed cmake list
     * moved all tests to ./tests directory
     * tests are linked against gtest_main
     * CI scripts adjustments
    
    Relates-to: OLPEDGE-591
    Signed-off-by: Serhii Lysenko <ext-serhii.lysenko@here.com>

--- scripts/linux/psv/travis_test_psv.sh
+++ scripts/linux/psv/travis_test_psv.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
-CPP_TEST_SOURCE_AUTHENTICATION=build/olp-cpp-sdk-authentication/test
+CPP_TEST_SOURCE_AUTHENTICATION=build/olp-cpp-sdk-authentication/tests
 CPP_TEST_SOURCE_CORE=build/olp-cpp-sdk-core/tests
 CPP_TEST_SOURCE_DARASERVICE_READ=build/olp-cpp-sdk-dataservice-read/test
 CPP_TEST_SOURCE_DARASERVICE_WRITE=build/olp-cpp-sdk-dataservice-write/test
 echo ">>> Authentication Test ... >>>"
-$CPP_TEST_SOURCE_AUTHENTICATION/unit/olp-authentication-test --gtest_output="xml:report.xml" --gtest_filter="AuthenticationOfflineTest.*"
+$CPP_TEST_SOURCE_AUTHENTICATION/olp-cpp-sdk-authentication-tests \
+    --gtest_output="xml:report.xml" \
+    --gtest_filter="AuthenticationOfflineTest.*"
 echo ">>> Core Test ... >>>"
 $CPP_TEST_SOURCE_CORE/olp-cpp-sdk-core-tests --gtest_output="xml:report1.xml"
 echo ">>> Dataservice read Test ... >>>"
