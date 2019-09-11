30a7177f9d8bed72a45264a8f2810c1bacedb8ff
Serhii Lysenko <ext-serhii.lysenko@here.com>
2019-09-11 18:11:06 +0300

    Adapt dataservice-read tests to Pitch-Fork form
     * fixed cmake list
     * moved all tests to ./tests directory
     * tests are linked against gtest_main
     * CI scripts adjustments
     * fix test report file names
    Relates-to: OLPEDGE-591, OLPEDGE-593
    Signed-off-by: Serhii Lysenko <ext-serhii.lysenko@here.com>

--- scripts/linux/psv/travis_test_psv.sh
+++ scripts/linux/psv/travis_test_psv.sh
@@ -1,16 +1,19 @@
 #!/bin/bash -e
 CPP_TEST_SOURCE_AUTHENTICATION=build/olp-cpp-sdk-authentication/tests
 CPP_TEST_SOURCE_CORE=build/olp-cpp-sdk-core/tests
-CPP_TEST_SOURCE_DARASERVICE_READ=build/olp-cpp-sdk-dataservice-read/test
+CPP_TEST_SOURCE_DARASERVICE_READ=build/olp-cpp-sdk-dataservice-read/tests
 CPP_TEST_SOURCE_DARASERVICE_WRITE=build/olp-cpp-sdk-dataservice-write/tests
 echo ">>> Authentication Test ... >>>"
 $CPP_TEST_SOURCE_AUTHENTICATION/olp-cpp-sdk-authentication-tests \
-    --gtest_output="xml:report.xml" \
+    --gtest_output="xml:olp-cpp-sdk-authentication-tests-report.xml" \
     --gtest_filter="AuthenticationOfflineTest.*"
 echo ">>> Core Test ... >>>"
-$CPP_TEST_SOURCE_CORE/olp-cpp-sdk-core-tests --gtest_output="xml:report1.xml"
+$CPP_TEST_SOURCE_CORE/olp-cpp-sdk-core-tests \
+    --gtest_output="xml:olp-cpp-sdk-core-tests-report.xml"
 echo ">>> Dataservice read Test ... >>>"
-$CPP_TEST_SOURCE_DARASERVICE_READ/unit/olp-dataservice-read-test --gtest_output="xml:report2.xml" --gtest_filter=-"TestOnline/*"
+$CPP_TEST_SOURCE_DARASERVICE_READ/olp-cpp-sdk-dataservice-read-tests \
+    --gtest_output="xml:olp-cpp-sdk-dataservice-read-tests-report.xml" \
+    --gtest_filter=-"TestOnline/*"
 echo ">>> Dataservice write Test ... >>>"
 $CPP_TEST_SOURCE_DARASERVICE_WRITE/olp-cpp-sdk-dataservice-write-tests \
     --gtest_output="xml:olp-cpp-sdk-dataservice-write-tests-report.xml" \
