ebc8f9be0fea175f6d1aa982ce85896331eaf78c
Serhii Lysenko <ext-serhii.lysenko@here.com>
2019-09-13 20:00:31 +0300

    Fix test names in FV CI scripts
    Relates-To: OLPEDGE-594
    Signed-off-by: Serhii Lysenko <ext-serhii.lysenko@here.com>

--- scripts/linux/fv/gitlab-olp-cpp-sdk-authentication-test.sh
+++ scripts/linux/fv/gitlab-olp-cpp-sdk-authentication-test.sh
@@ -2,4 +2,6 @@
 
 echo ">>> Core - Authentication Test ... >>>"
 source $FV_HOME/olp-cpp-sdk-core-authentication-test.variables
-$REPO_HOME/build/olp-cpp-sdk-authentication/test/unit/olp-authentication-test --gtest_output="xml:$REPO_HOME/reports/olp-authentication-test-report.xml" --gtest_filter="-ArcGisAuthenticationOnlineTest.SignInArcGis"
+$REPO_HOME/build/olp-cpp-sdk-authentication/tests/olp-cpp-sdk-authentication-tests \
+    --gtest_output="xml:$REPO_HOME/reports/olp-authentication-test-report.xml" \
+    --gtest_filter="-ArcGisAuthenticationOnlineTest.SignInArcGis"
--- scripts/linux/fv/gitlab-olp-dataservice-read-test.sh
+++ scripts/linux/fv/gitlab-olp-dataservice-read-test.sh
@@ -2,4 +2,5 @@
 
 echo ">>> Core - SOURCE DATASERVICE READ Test ... >>>"
 source $FV_HOME/olp-dataservice-read-test.variables
-$REPO_HOME/build/olp-cpp-sdk-dataservice-read/test/unit/olp-dataservice-read-test --gtest_output="xml:$REPO_HOME/reports/olp-dataservice-read-test-report.xml"
+$REPO_HOME/build/olp-cpp-sdk-dataservice-read/tests/olp-cpp-sdk-dataservice-read-tests \
+    --gtest_output="xml:$REPO_HOME/reports/olp-dataservice-read-test-report.xml"
