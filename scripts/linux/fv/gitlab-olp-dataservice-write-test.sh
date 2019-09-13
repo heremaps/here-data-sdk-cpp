12e059879bbdcd090232cdf4e0b5edb4f5c32454
Serhii Lysenko <ext-serhii.lysenko@here.com>
2019-09-13 20:00:31 +0300

    Fix test names in FV CI scripts
    Relates-To: OLPEDGE-594
    Signed-off-by: Serhii Lysenko <ext-serhii.lysenko@here.com>

--- scripts/linux/fv/gitlab-olp-dataservice-write-test.sh
+++ scripts/linux/fv/gitlab-olp-dataservice-write-test.sh
@@ -2,4 +2,5 @@
 
 echo ">>> Core - SOURCE DATASERVICE WRITE Test ... >>>"
 source $FV_HOME/olp-dataservice-write-test.variables
-$REPO_HOME/build/olp-cpp-sdk-dataservice-write/test/olp-dataservice-write-test --gtest_output="xml:$REPO_HOME/reports/olp-dataservice-write-test-report.xml"
+$REPO_HOME/build/olp-cpp-sdk-dataservice-write/tests/olp-cpp-sdk-dataservice-write-tests \
+    --gtest_output="xml:$REPO_HOME/reports/olp-dataservice-write-test-report.xml"
