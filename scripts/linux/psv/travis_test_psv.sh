e77380e9264d55ee3494a1d64574f82fd4feb843
Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>
2019-09-16 10:14:33 +0300

    Added a localhost server.
    Resolves: OLPEDGE-716
    
    Signed-off-by: Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>

--- scripts/linux/psv/travis_test_psv.sh
+++ scripts/linux/psv/travis_test_psv.sh
@@ -3,6 +3,7 @@ CPP_TEST_SOURCE_AUTHENTICATION=build/olp-cpp-sdk-authentication/tests
 CPP_TEST_SOURCE_CORE=build/olp-cpp-sdk-core/tests
 CPP_TEST_SOURCE_DARASERVICE_READ=build/olp-cpp-sdk-dataservice-read/tests
 CPP_TEST_SOURCE_DARASERVICE_WRITE=build/olp-cpp-sdk-dataservice-write/tests
+CPP_TEST_SOURCE_FUNCTIONAL=build/tests/functional
 echo ">>> Authentication Test ... >>>"
 $CPP_TEST_SOURCE_AUTHENTICATION/olp-cpp-sdk-authentication-tests \
     --gtest_output="xml:olp-cpp-sdk-authentication-tests-report.xml" \
@@ -18,5 +19,10 @@ echo ">>> Dataservice write Test ... >>>"
 $CPP_TEST_SOURCE_DARASERVICE_WRITE/olp-cpp-sdk-dataservice-write-tests \
     --gtest_output="xml:olp-cpp-sdk-dataservice-write-tests-report.xml" \
     --gtest_filter=-"*Online*":"TestCacheMock*"
+echo ">>> Functional Test ... >>>"
+node ./testutils/mock_server/server.js & SERVER_PID=$!
+$CPP_TEST_SOURCE_FUNCTIONAL/olp-cpp-sdk-functional-tests \
+    --gtest_output="xml:olp-cpp-sdk-functional-tests-report.xml"
+kill -15 $SERVER_PID
 
 bash <(curl -s https://codecov.io/bash)
---
+++ tests/functional/olp-cpp-sdk-core/NetworkTest.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+#include <gtest/gtest.h>
+#include <olp/core/http/HttpStatusCode.h>
+#include <olp/core/http/Network.h>
+#include <olp/core/logging/Log.h>
+
+#include <algorithm>
+#include <atomic>
+#include <condition_variable>
+#include <thread>
+
+TEST(NetworkTest, GetRequest) {
+  auto network = olp::http::CreateDefaultNetwork(1);
+
+  auto payload = std::make_shared<std::stringstream>();
+
+  std::mutex m;
+  std::condition_variable cv;
+
+  auto main_thread_id = std::this_thread::get_id();
+
+  olp::http::NetworkRequest request("http://localhost:3000/get_request");
+
+  auto outcome = network->Send(
+      request, payload,
+      [payload, &cv,
+       main_thread_id](const olp::http::NetworkResponse& response) {
+        EXPECT_EQ(response.GetStatus(), olp::http::HttpStatusCode::OK);
+        EXPECT_EQ(payload->str(), "GET handler");
+        EXPECT_NE(main_thread_id, std::this_thread::get_id());
+        cv.notify_one();
+      });
+
+  ASSERT_TRUE(outcome.IsSuccessfull());
+
+  std::unique_lock<std::mutex> lock(m);
+  ASSERT_TRUE(cv.wait_for(lock, std::chrono::seconds(1)) ==
+              std::cv_status::no_timeout);
+
+  // At this moment there must be only 1 pointer to the network
+  ASSERT_EQ(network.use_count(), 1);
+}
