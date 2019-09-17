54cd0948dcbd94da1ef36bf1a4bfff932fc76fb1
Serhii Lysenko <ext-serhii.lysenko@here.com>
2019-09-17 14:39:06 +0300

    Add env variables for authentication FV tests
    Resolves: OLPEDGE-790
    
    Signed-off-by: Serhii Lysenko <ext-serhii.lysenko@here.com>

--- scripts/linux/fv/gitlab-olp-cpp-sdk-authentication-test.sh
+++ scripts/linux/fv/gitlab-olp-cpp-sdk-authentication-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo ">>> Core - Authentication Test ... >>>"
-source $FV_HOME/olp-cpp-sdk-core-authentication-test.variables
+source $FV_HOME/olp-cpp-sdk-authentication-test.variables
 $REPO_HOME/build/olp-cpp-sdk-authentication/tests/olp-cpp-sdk-authentication-tests \
     --gtest_output="xml:$REPO_HOME/reports/olp-authentication-test-report.xml" \
     --gtest_filter="-ArcGisAuthenticationOnlineTest.SignInArcGis"
