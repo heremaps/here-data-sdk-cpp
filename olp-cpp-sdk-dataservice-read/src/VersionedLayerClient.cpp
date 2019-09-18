b899cdf3d06c2222f451ed8aefc62620c9f1dfaf
Serhii Lysenko <ext-serhii.lysenko@here.com>
2019-09-18 17:44:31 +0300

    VersionedLayerClient draft
    Signed-off-by: Serhii Lysenko <ext-serhii.lysenko@here.com>

--- olp-cpp-sdk-dataservice-read/src/VersionedLayerClient.cpp
+++ olp-cpp-sdk-dataservice-read/src/VersionedLayerClient.cpp
@@ -3,14 +3,50 @@
 #include <olp/core/client/OlpClientFactory.h>
 #include <olp/core/thread/TaskScheduler.h>
 
-// #include "ExecuteOrSchedule.inl"
-
 #include "ApiClientLookup.h"
 #include "generated/model/Api.h"
 
 #include <olp/core/client/HRN.h>
 #include <olp/core/client/OlpClient.h>
-namespace {}  // end of namespace
+#include <olp/core/context/Context.h>
+
+#include "generated/api/BlobApi.h"
+#include "generated/api/MetadataApi.h"
+
+namespace {
+
+class Condition {
+ public:
+  Condition(olp::client::CancellationContext context, int timeout = 60)
+      : context_{context}, timeout_{timeout}, signaled_{false} {}
+
+  void Notify() {
+    {
+      std::unique_lock<std::mutex> lock(mutex_);
+      signaled_ = true;
+    }
+    condition_.notify_one();
+  }
+
+  bool Wait() {
+    std::unique_lock<std::mutex> lock(mutex_);
+    bool triggered = condition_.wait_for(
+        lock, std::chrono::seconds(timeout_),
+        [&] { return signaled_ || context_.IsCancelled(); });
+
+    signaled_ = false;
+    return triggered;
+  }
+
+ private:
+  olp::client::CancellationContext context_;
+  int timeout_;
+  std::condition_variable condition_;
+  std::mutex mutex_;
+  bool signaled_;
+};
+
+}  // end of namespace
 
 namespace olp {
 namespace dataservice {
@@ -30,13 +66,95 @@ VersionedLayerClient::VersionedLayerClient(
 
 olp::client::CancellationToken VersionedLayerClient::GetDataByPartitionId(
     std::string partition_id, DataResponseCallback callback) {
-  ApiClientLookup::ApisResponse apis_response;
+  olp::client::CancellationContext context;
+  olp::client::CancellationToken token(
+      [=]() mutable { context.CancelOperation(); });
+  Condition condition(context);
+  auto wait_and_check = [&] {
+    if (!condition.Wait() && !context.IsCancelled()) {
+      callback({{olp::client::ErrorCode::RequestTimeout,
+                 "Network request timed out.", true}});
+      return false;
+    }
+    return true;
+  };
 
-  ApiClientLookup::LookupApi(olp_client_, "query", "v1",
-                             olp::client::HRN::FromString(hrn_),
-                             [&](ApiClientLookup::ApisResponse response) {
-                               apis_response = response;
-                             });
+  // Step 1.
+
+  ApiClientLookup::ApiClientResponse apis_response;
+
+  context.ExecuteOrCancelled([&]() {
+    return ApiClientLookup::LookupApiClient(
+        olp_client_, "query", "v1", olp::client::HRN::FromString(hrn_),
+        [&](ApiClientLookup::ApiClientResponse response) {
+          apis_response = std::move(response);
+          condition.Notify();
+        });
+  });
+
+  if (!apis_response.IsSuccessful() || !wait_and_check()) {
+    return token;
+  }
+
+  auto query_client = apis_response.GetResult();
+
+  // Step 2.
+
+  MetadataApi::PartitionsResponse partitions_response;
+  context.ExecuteOrCancelled([&]() {
+    return olp::dataservice::read::MetadataApi::GetPartitions(
+        query_client, layer_id_, layer_version_, boost::none, boost::none,
+        boost::none, [&](MetadataApi::PartitionsResponse response) {
+          partitions_response = std::move(response);
+          condition.Notify();
+        });
+  });
+
+  if (!partitions_response.IsSuccessful() || !wait_and_check()) {
+    return token;
+  }
+
+  auto partitions = partitions_response.GetResult();
+
+  // Step 3.
+
+  context.ExecuteOrCancelled([&]() {
+    return ApiClientLookup::LookupApiClient(
+        olp_client_, "blob", "v1", olp::client::HRN::FromString(hrn_),
+        [&](ApiClientLookup::ApiClientResponse response) {
+          apis_response = std::move(response);
+          condition.Notify();
+        });
+  });
+
+  if (!apis_response.IsSuccessful() || !wait_and_check()) {
+    return token;
+  }
+
+  auto blob_client = apis_response.GetResult();
+
+  // Step 4.
+  // TODO: check partitions is not empty
+
+  auto data_handle = partitions.GetPartitions().at(0).GetDataHandle();
+
+  BlobApi::DataResponse data_response;
+  context.ExecuteOrCancelled([&]() {
+    return olp::dataservice::read::BlobApi::GetBlob(
+        blob_client, layer_id_, data_handle, boost::none, boost::none,
+        [&](BlobApi::DataResponse response) {
+          data_response = std::move(response);
+          condition.Notify();
+        });
+  });
+
+  if (!data_response.IsSuccessful() || !wait_and_check()) {
+    return token;
+  }
+
+  auto data = data_response.GetResult();
+
+  callback(data);
 }
 
 }  // namespace read
