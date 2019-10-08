5878d01e0fa222551a6bd7d9a3307376115765ab
Diachenko Mykahilo <ext-mykhailo.z.diachenko@here.com>
2019-10-08 16:40:40 +0300

    Add VolatileLayerClient class.
    Create VolatileLayerClient and VolatileLayerClientImpl classes:
    - document usage of VolatileLayerClient
    - add test skeletons for VolatileLayerClient and VolatileLayerClientImpl
    
    Relates-to: OLPEDGE-758
    Signed-off-by: Diachenko Mykahilo <ext-mykhailo.z.diachenko@here.com>

---
+++ olp-cpp-sdk-dataservice-read/tests/VolatileLayerClientImplTest.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+#include <gtest/gtest.h>
+#include "VolatileLayerClientImpl.h"
+
+namespace {
+using namespace olp::dataservice::read;
+using namespace ::testing;
+const std::string kCatalog = "hrn:here:data:::hereos-internal-test-v2";
+const std::string kLayerId = "olp-sdk-test";
+const auto kHRN = olp::client::HRN::FromString(kCatalog);
+
+TEST(VolatileLayerClientImplTest, GetData) {
+  olp::client::OlpClientSettings settings;
+
+  VolatileLayerClientImpl client(kHRN, kLayerId, settings);
+
+  DataRequest request;
+
+  VolatileLayerClientImpl::Callback cb;
+  auto token = client.GetData(request, cb);
+  token.cancel();
+}
+}  // namespace
