8ca08c9d2d973b754b0b1f595b0b06bfe04ca010
Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>
2019-10-15 13:37:58 +0300

    Fix examples compilation on psv.
    Resolves: OLPEDGE-887
    
    Signed-off-by: Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>

--- scripts/linux/psv/travis_build_psv.sh
+++ scripts/linux/psv/travis_build_psv.sh
@@ -2,7 +2,7 @@
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -OLP_SDK_BUILD_EXAMPLES=ON \
+    -DOLP_SDK_BUILD_EXAMPLES=ON \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
     ..
