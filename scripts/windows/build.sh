ab6d10b2a5e5595b012ba2ee0e5197fd4c94d27e
Diachenko Mykahilo <ext-mykhailo.z.diachenko@here.com>
2019-11-15 14:37:35 +0200

    Add parallel build option for Visual Studio
    Relates-To: OLPEDGE-0000
    Signed-off-by: Diachenko Mykahilo <ext-mykhailo.z.diachenko@here.com>

--- scripts/windows/build.sh
+++ scripts/windows/build.sh
@@ -1,5 +1,6 @@
 [[ -d "build" ]] && rm -rf build
 mkdir build && cd build
-cmake .. -G "Visual Studio 15 2017 Win64" \
+cmake .. -DCMAKE_CXX_FLAGS=" /MP" \
+         -G "Visual Studio 15 2017 Win64" \
          -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 cmake --build . --config $BUILD_TYPE
