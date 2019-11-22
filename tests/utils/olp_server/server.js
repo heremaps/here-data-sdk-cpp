71d2290b6e30c519231c2459cfecc224c4958b6f
Diachenko Mykahilo <ext-mykhailo.z.diachenko@here.com>
2019-11-22 15:41:42 +0200

    Add errors and timeouts to mock server
    Add emulation of errors and timeouts for mock server. This behavior is
    controlled by 'debug' header in http requests. Network wrapper for
    performance test implements interface to set this header.
    
    Relates-To: OLPEDGE-1115
    Signed-off-by: Diachenko Mykahilo <ext-mykhailo.z.diachenko@here.com>

--- tests/utils/olp_server/server.js
+++ tests/utils/olp_server/server.js
@@ -26,14 +26,42 @@ const config_service_handler = require('./config_service.js')
 const metadata_service_handler = require('./metadata_service.js')
 const query_service_handler = require('./query_service.js')
 const blob_service_handler = require('./blob_service.js')
+const errors_generator = require('./errors_generator.js')
 
 const port = 3000
 
-const sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
+function sleep(milliseconds) {
+  var waitTill = new Date(new Date().getTime() + milliseconds);
+  while (waitTill > new Date()) {
+  }
 }
 
-const handlers = {}
+function handleRequest(response, pathname, request, handler) {
+  const result = handler(pathname, request)
+  response.writeHead(result.status, result.headers)
+  response.end(result.text)
+}
+
+function errorsDecorator(processor) {
+  return function (response, pathname, request, handler) {
+    if (Math.floor(Math.random() * 100) > 10) {
+      processor(response, pathname, request, handler);
+    } else {
+      processor(response, pathname, request, errors_generator.handler);
+    }
+  }
+}
+
+function timeoutDecorator(processor) {
+  return function (response, pathname, request, handler) {
+    milliseconds = Math.floor(Math.random() * 250);
+    console.log('Waiting for ' + milliseconds + 'ms');
+    sleep(milliseconds);
+    processor(response, pathname, request, handler);
+  }
+}
+
+const handlers = {};
 handlers[services.lookup] = lookup_service_handler.handler
 handlers[services.config] = config_service_handler.handler
 handlers[services.metadata] = metadata_service_handler.handler
@@ -42,54 +70,64 @@ handlers[services.blob] = blob_service_handler.handler
 
 const requestHandler = async (request, response) => {
 
-    request.on('error', (err) => {
-        console.error(err);
-        response.writeHead(400, {}).end();
-    });
-    response.on('error', (err) => {
-        console.error(err);
-    });
+  request.on('error', (err) => {
+    console.error(err);
+    response.writeHead(400, {}).end();
+  });
+  response.on('error', (err) => {
+    console.error(err);
+  });
 
-    const { headers, method, url } = request;
-    
-    // Currently we support only read operations
-    if (method != 'GET') {
-        response.writeHead(404, {})
-        response.end('Not Found')
-        return
-    }
-    
-    // For debug purpose
-    console.log(url)
-    
-    const { host, query, pathname } = URL.parse(url, true)
-    
-    const handler = handlers[host]
-    if (handler) {
-        const result = handler(pathname, query)
-        response.writeHead(result.status, result.headers)
-        response.end(result.text)
-        return
-    }
-    
-    response.writeHead(400, {})
-    response.end('Not implemented')
+  const { headers, method, url } = request;
+
+  // Currently we support only read operations
+  if (method != 'GET') {
+    response.writeHead(404, {})
+    response.end('Not Found')
+    return
+  }
+
+  // For debug purpose
+  console.log(url)
+
+  processor = handleRequest
+
+  if (headers['debug-with-errors']) {
+    console.log('This one will be decorated with errors')
+    processor = errorsDecorator(processor)
+  }
+
+  if (headers['debug-with-timeouts']) {
+    console.log('This one will be decorated with timeouts')
+    processor = timeoutDecorator(processor)
+  }
+
+  const { host, query, pathname } = URL.parse(url, true)
+
+  const handler = handlers[host]
+  if (handler) {
+    processor(response, pathname, query, handler)
+    return
+  }
+
+  response.writeHead(400, {})
+  response.end('Not implemented')
 }
 
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
+  if (err) {
+    return console.log('something bad happened', err)
+  }
 
-    console.log(`server is listening on ${port}`)
+  console.log(`server is listening on ${port}`)
 })
 
 // Handle termination by Travis
 process.on('SIGTERM', function () {
-    server.close(function () {
-        console.log(`Graceful shutdown`)
-        process.exit(0);
-    });
+  server.close(function () {
+    console.log(`Graceful shutdown`)
+    process.exit(0);
+  });
 });
