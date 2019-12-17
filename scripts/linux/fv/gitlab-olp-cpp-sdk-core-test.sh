40ff29f9433832eac1d75e1367838d78742ae537
Dmytro Poberezhnyi <ext-dmytro.poberezhnyi@here.com>
2019-12-17 13:19:22 +0200

    Added DefaultCacheTest.BadPathMutable to black list.
    DefaultCacheTest.BadPathMutable(initially DefaultCacheTest.BadPath) was already in black list
    but this has been lost after its renaming. Restoring.
    
    Resolves: OLPEDGE-1266
    
    Signed-off-by: Dmytro Poberezhnyi <ext-dmytro.poberezhnyi@here.com>

--- scripts/linux/fv/gitlab-olp-cpp-sdk-core-test.sh
+++ scripts/linux/fv/gitlab-olp-cpp-sdk-core-test.sh
@@ -22,4 +22,4 @@ echo ">>> Core Test ... >>>"
 source $FV_HOME/olp-cpp-sdk-core-test.variables
 $REPO_HOME/build/olp-cpp-sdk-core/tests/olp-cpp-sdk-core-tests \
     --gtest_output="xml:$REPO_HOME/reports/olp-core-test-report.xml" \
-    --gtest_filter=-"DefaultCacheTest.BadPath"
+    --gtest_filter=-"DefaultCacheTest.BadPathMutable"
