6b3e90d3a82d2701de6c428cafbe9b24bb517c89
Kirill Zhuchkov <kirill.zhuchkov@here.com>
2020-01-07 10:57:47 +0100

    Add unit tests for olp::logging::Configuration class
    Introducing also a simple MockAppender that will be used
    as a message sink for testing this and other logging classes.
    
    Relates-To: OLPEDGE-1237
    
    Signed-off-by: Kirill Zhuchkov <kirill.zhuchkov@here.com>

---
+++ olp-cpp-sdk-core/tests/logging/ConfigurationTest.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2020 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+#include <gtest/gtest.h>
+#include <olp/core/logging/Configuration.h>
+#include <olp/core/logging/Log.h>
+#include <memory>
+#include "MockAppender.h"
+
+namespace {
+
+using namespace olp::logging;
+using namespace testing;
+
+TEST(ConfigurationTest, InvalidConfiguration) {
+  Configuration configuration;
+  EXPECT_FALSE(configuration.isValid());
+  EXPECT_FALSE(Log::configure(configuration));
+  OLP_SDK_LOG_TRACE("My tag", "My message");
+}
+
+TEST(ConfigurationTest, DefaultConfiguration) {
+  Configuration configuration = Configuration::createDefault();
+  EXPECT_TRUE(configuration.isValid());
+  EXPECT_TRUE(Log::configure(configuration));
+  OLP_SDK_LOG_TRACE("My tag", "My message");
+}
+
+TEST(ConfigurationTest, MockAppender) {
+  Configuration configuration;
+  configuration.addAppender(std::make_shared<MockAppender>());
+  EXPECT_TRUE(configuration.isValid());
+  EXPECT_TRUE(Log::configure(configuration));
+  OLP_SDK_LOG_TRACE("My tag", "My message");
+}
+
+}  // namespace
---
+++ olp-cpp-sdk-core/tests/logging/MockAppender.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2020 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+#pragma once
+
+#include <olp/core/logging/Appender.h>
+#include <string>
+#include <vector>
+
+namespace testing {
+
+using namespace olp::logging;
+
+class MockAppender : public IAppender {
+ public:
+  struct MessageData {
+    Level level_;
+    std::string tag_;
+    std::string message_;
+    std::string file_;
+    unsigned int line_;
+    std::string function_;
+  };
+
+  IAppender& append(const LogMessage& message) override;
+
+  std::vector<MessageData> messages_;
+};
+
+}  // namespace testing
