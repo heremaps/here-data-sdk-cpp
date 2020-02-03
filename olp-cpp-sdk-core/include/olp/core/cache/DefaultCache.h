340a7ab9079b77ef1f8d6053c0922379cca41eee
HalynaDumych <56076012+HalynaDumych@users.noreply.github.com>
2020-02-03 10:56:32 +0200

    Modify documentation (core/include/cache) (#628)
    Relates-to: OLPEDGE-860
    
    Signed-off-by: Halyna Dumych <ext-halyna.dumych@here.com>

--- olp-cpp-sdk-core/include/olp/core/cache/DefaultCache.h
+++ olp-cpp-sdk-core/include/olp/core/cache/DefaultCache.h
@@ -33,12 +33,12 @@ class DiskCache;
 
 /**
  * @brief A default cache that provides an in-memory LRU cache and persistence
- * of the cached key-value pairs.
+ * of cached key-value pairs.
  *
  * @note By default, the downloaded data is cached only in memory.
  * To enable the persistent cache, define
- * `olp::cache::CacheSettings::disk_path`. On iOS, the path is relative to the
- * application data folder.
+ * `olp::cache::CacheSettings::disk_path`. On iOS, the path is relative to
+ * the application data folder.
  *
  * The default maximum size of the persistent cache is 32 MB. If the entire
  * available disk space should be used, set
@@ -55,86 +55,98 @@ class CORE_API DefaultCache : public KeyValueCache {
  public:
   /*! The storage open result type */
   enum StorageOpenResult {
-    Success,            /*!< operation succeeded */
-    OpenDiskPathFailure /*!< disk cache failure */
+    Success,            /*!< The operation succeeded. */
+    OpenDiskPathFailure /*!< The disk cache failure. */
   };
 
   /**
-   * @brief Parameterized constructor.
-   * @param settings Settings for the cache.
+   * @brief Creates the `DefaultCache` instance.
+   *
+   * @param settings The cache settings.
    */
   DefaultCache(const CacheSettings& settings = CacheSettings());
   ~DefaultCache() override;
 
   /**
-   * @brief Opens the cache to start read/write operations.
+   * @brief Opens the cache to start read and write operations.
    *
-   * @return StorageOpenResult if there were problems opening any of the
-   * provided pathes on the disk.
+   * @return `StorageOpenResult` if there are problems opening any of
+   * the provided paths on the disk.
    */
   StorageOpenResult Open();
 
   /**
-   * @brief Closes the cache for use.
+   * @brief Closes the cache.
    */
   void Close();
 
   /**
-   * @brief Clears the contents of the cache.
-   * @return Returns true if the operation is successfull, false otherwise.
+   * @brief Clears the cache content.
+   *
+   * @return True if the operation is successful; false otherwise.
    */
   bool Clear();
 
   /**
-   * @brief Stores the key-value pair into the cache.
-   * @param key Key for this value
-   * @param value The value of any type
-   * @param encoder A method provided to encode the specified value into a
-   * string
-   * @param expiry Time in seconds to when pair will expire
-   * @return Returns true if the operation is successfull, false otherwise.
+   * @brief Stores the key-value pair in the cache.
+   *
+   * @param key The key for this value.
+   * @param value The value of any type.
+   * @param encoder Encodes the specified value into a string.
+   * @param expiry The expiry time (in seconds) of the key-value pair.
+   *
+   * @return True if the operation is successful; false otherwise.
    */
   bool Put(const std::string& key, const boost::any& value,
            const Encoder& encoder, time_t expiry) override;
 
   /**
-   * @brief Stores the raw binary data as value into the cache.
-   * @param key Key for this value
-   * @param value binary data to be stored
-   * @param expiry Time in seconds to when pair will expire
-   * @return Returns true if the operation is successfull, false otherwise.
+   * @brief Stores the raw binary data as a value in the cache.
+   *
+   * @param key The key for this value.
+   * @param value The binary data that should be stored.
+   * @param expiry The expiry time (in seconds) of the key-value pair.
+   *
+   * @return True if the operation is successful; false otherwise.
    */
   bool Put(const std::string& key, const KeyValueCache::ValueTypePtr value,
            time_t expiry) override;
 
   /**
    * @brief Gets the key-value pair from the cache.
-   * @param key Key to look for
-   * @param decoder A method is provided to decode a value from a string if
-   * needed
+   *
+   * @param key The key that is used to look for the key-value pair.
+   * @param decoder Decodes the value from a string.
+   *
+   * @return The key-value pair.
    */
   boost::any Get(const std::string& key, const Decoder& decoder) override;
 
   /**
    * @brief Gets the key and binary data from the cache.
-   * @param key Key to look for
+   *
+   * @param key The key that is used to look for the binary data.
+   *
+   * @return The key and binary data.
    */
   KeyValueCache::ValueTypePtr Get(const std::string& key) override;
 
   /**
    * @brief Removes the key-value pair from the cache.
-   * @param key Key for the value to remove from cache
    *
-   * @return Returns true if the operation is successfull, false otherwise.
+   * @param key The key for the value.
+   *
+   * @return True if the operation is successful; false otherwise.
    */
   bool Remove(const std::string& key) override;
 
   /**
-   * @brief Removes the values with the keys matching the given
+   * @brief Removes the values with the keys that match the given
    * prefix from the cache.
-   * @param prefix Prefix to look for
    *
-   * @return Returns true on removal, false otherwise.
+   * @param prefix The prefix that matches the keys.
+   *
+   * @return True if the values are removed; false otherwise.
    */
   bool RemoveKeysWithPrefix(const std::string& prefix) override;
 
