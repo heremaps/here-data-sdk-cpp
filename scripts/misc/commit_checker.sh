36deaceb77a923d262bad1de3895143ad7b1a3aa
Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>
2020-04-13 20:27:43 +0300

    Minor fixes to commit checker job (#773)
    Minor fixes to comments and removed checks for branch
    as job runs on PullRequests only.
    
    Relates-To: OLPEDGE-1573
    
    Signed-off-by: Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>

--- scripts/misc/commit_checker.sh
+++ scripts/misc/commit_checker.sh
@@ -18,20 +18,12 @@
 # License-Filename: LICENSE
 #
 
-# This is script for verifying commit message by SDK requirements.
+# This is script for verifying commit message by requirements.
 # Best git and linux practices are included.
 # Requirements can be find in scripts/misc/commit_message_recom.txt
 
 # Saving whole commit message into file for reading below
-set -x
-git branch
-branch_name="$(git branch | grep \* | cut -d ' ' -f5| cut -d ')' -f1)"
-if [[ "$branch_name" =~ "pull" ]] ; then
-    echo "`git log --pretty=format:'%B' -2 | sed '1d' | sed '1d' `" >> commit.log
-else
-    echo "`git log --pretty=format:'%B' -1`" >> commit.log
-fi
-set +x
+echo "`git log --pretty=format:'%B' -2 | sed '1d' | sed '1d' `" >> commit.log
 
 # Counting number of lines in file
 num_lines=`wc -l commit.log| cut -d'c' -f1` 
@@ -41,34 +33,54 @@ for (( line=1; line<=${num_lines}; line++ ))
 do
     current_line_len=`cat commit.log| sed -n ${line}p |wc -m | sed 's/\ \ \ \ \ \ /''/g' `
     if [ $line -eq 1 ] && [ ${current_line_len} -gt 50 ] ; then
-        echo "ERROR: Title is ${current_line_len} length, so it's too long for title. It must be less than 50 chars "
+        echo ""
+        echo "ERROR: Title is ${current_line_len} length, so it's too long for title. Expect less than 50 chars !"
+        echo ""
+        echo "Please read following rules:"
+        cat scripts/misc/commit_message_recom.txt
         exit 1
     fi
     if [ $line -eq 2 ] && [ ${current_line_len} -ne 1 ] ; then
-        echo "ERROR: Second line in Commit Message is not zero length"
+        echo ""
+        echo "ERROR: Second line in Commit Message is not zero length !"
+        echo ""
+        echo "Please read following rules:"
+        cat scripts/misc/commit_message_recom.txt
         exit 1
     fi
     if [ $line -eq 3 ] && [ ${current_line_len} -lt 1 ] && [ -n $(cat commit.log| sed -n ${line}p | grep 'See also: ') ] && [ -n $(cat commit.log| sed -n ${line}p | grep 'Relates-To: ') ] && [ -n $(echo ${current_line_len}| grep 'Resolves: ') ] ; then
-        echo "ERROR: No details added to commit message besides title starting from 3rd line."
+        echo ""
+        echo "ERROR: No details added to commit message besides title !"
+        echo ""
+        echo "Please read following rules:"
+        cat scripts/misc/commit_message_recom.txt
         exit 1
     fi
     if [ ${current_line_len} -gt 72 ] ; then
-        echo "ERROR: ${current_line_len} chars in ${line}-th line is too long. This line must be less than 72 chars"
+        echo ""
+        echo "ERROR: ${current_line_len} chars in ${line}-th line is too long. Any line length must be less than 72 chars !"
+        echo ""
+        echo "Please read following rules:"
+        cat scripts/misc/commit_message_recom.txt
         exit 1
     fi
-    echo " ${line}-th line is ${current_line_len} chars length"
+    echo " ${line}-th line is ${current_line_len} chars length . OK."
 done
 
 relates_to=$(cat commit.log | grep 'Relates-To: ') || true
 resolves=$(cat commit.log | grep 'Resolves: ') || true
 see=$(cat commit.log | grep 'See also: ') || true
 
-echo "Reference like:  ${relates_to} ${resolves} ${see}  was found in commit message."
+echo "Reference like:  ${relates_to} ${resolves} ${see}  was found in commit message. OK."
 
 if [[ -n ${relates_to} || -n ${resolves} || -n ${see} ]] ; then
-    echo "Commit message contains Jira link."
+    echo ""
+    echo "Commit message contains issue reference. OK."
+    echo ""
 else
-    echo "ERROR: Commit message does not contain ticket reference like Relates-To: or Resolves: or See also:  "
+    echo ""
+    echo "ERROR: Commit message does not contain ticket or issue reference like Relates-To: or Resolves: or See also:  !"
+    echo ""
     echo "Please read following rules:"
     cat scripts/misc/commit_message_recom.txt
     exit 1
