018e85b6562e8aea2bff7fface4c95c1760dd75b
Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>
2020-04-23 16:54:59 +0300

    Add prefetch memory tests.
    Test performs a N parallel requests with configurable number of tiles
    Test is designed to estimate the memory consumption, and compare
    different cache settings.
    
    Relates-To: OLPEDGE-1804
    
    Signed-off-by: Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>

--- tests/performance/PrefetchTest.cpp
+++ tests/performance/PrefetchTest.cpp
@@ -32,10 +32,8 @@ using TestFunction = std::function<void(uint8_t thread_id)>;
 
 struct TestConfiguration : public TestBaseConfiguration {
   std::string configuration_name;
-  std::uint16_t requests_per_second = 3;
   std::uint8_t calling_thread_count = 5;
-  std::chrono::seconds runtime = std::chrono::minutes(5);
-  float cancelation_chance = 0.f;
+  std::uint16_t number_of_tiles = 100;
 };
 
 std::ostream& operator<<(std::ostream& os, const TestConfiguration& config) {
@@ -43,35 +41,20 @@ std::ostream& operator<<(std::ostream& os, const TestConfiguration& config) {
             << ".configuration_name=" << config.configuration_name
             << ", .calling_thread_count=" << config.calling_thread_count
             << ", .task_scheduler_capacity=" << config.task_scheduler_capacity
-            << ", .requests_per_second=" << config.requests_per_second
-            << ", .runtime=" << config.runtime.count() << ")";
+            << ")";
 }
 
-constexpr std::chrono::milliseconds GetSleepPeriod(uint32_t requests) {
-  return std::chrono::milliseconds(1000 / requests);
-}
-
-bool ShouldCancel(const TestConfiguration& configuration) {
-  float cancel_chance = configuration.cancelation_chance;
-  cancel_chance = std::min(cancel_chance, 1.f);
-  cancel_chance = std::max(cancel_chance, 0.f);
-  int thresold = static_cast<int>(cancel_chance * 100);
-  return rand() % 100 <= thresold;
-}
-
-constexpr auto kLogTag = "MemoryTest";
+constexpr auto kLogTag = "PrefetchTest";
 const olp::client::HRN kCatalog("hrn:here:data::olp-here-test:testhrn");
 const std::string kVersionedLayerId("versioned_test_layer");
 
-class MemoryTest : public MemoryTestBase<TestConfiguration> {
+class PrefetchTest : public MemoryTestBase<TestConfiguration> {
  public:
   void SetUp() override;
   void TearDown() override;
 
   void StartThreads(TestFunction test_body);
   void ReportError(const olp::client::ApiError& error);
-
-  void RandomlyCancel(olp::client::CancellationToken token);
 
  protected:
   std::atomic<olp::http::RequestId> request_counter_;
@@ -85,7 +68,7 @@ class MemoryTest : public MemoryTestBase<TestConfiguration> {
   std::map<int, int> errors_;
 };
 
-void MemoryTest::SetUp() {
+void PrefetchTest::SetUp() {
   using namespace olp;
   request_counter_.store(
       static_cast<http::RequestId>(http::RequestIdConstants::RequestIdMin));
@@ -95,7 +78,7 @@ void MemoryTest::SetUp() {
   errors_.clear();
 }
 
-void MemoryTest::TearDown() {
+void PrefetchTest::TearDown() {
   for (auto& thread : client_threads_) {
     thread.join();
   }
@@ -116,7 +99,7 @@ void MemoryTest::TearDown() {
   EXPECT_EQ(total_requests_.load(), total_requests);
 }
 
-void MemoryTest::StartThreads(TestFunction test_body) {
+void PrefetchTest::StartThreads(TestFunction test_body) {
   const auto& parameter = GetParam();
 
   for (uint8_t i = 0; i < parameter.calling_thread_count; ++i) {
@@ -124,7 +107,7 @@ void MemoryTest::StartThreads(TestFunction test_body) {
   }
 }
 
-void MemoryTest::ReportError(const olp::client::ApiError& error) {
+void PrefetchTest::ReportError(const olp::client::ApiError& error) {
   const auto error_code = static_cast<int>(error.GetErrorCode());
 
   std::lock_guard<std::mutex> lock(errors_mutex_);
@@ -136,123 +119,48 @@ void MemoryTest::ReportError(const olp::client::ApiError& error) {
   }
 }
 
-void MemoryTest::RandomlyCancel(olp::client::CancellationToken token) {
-  const auto& parameter = GetParam();
-  if (ShouldCancel(parameter)) {
-    std::this_thread::sleep_for(std::chrono::microseconds(rand() % 3000));
-    token.Cancel();
-  }
-}
-
 ///
 /// VersionedLayerClient
 ///
-
-/*
- * Test performs N requests per second for M duration. All requests are
- * unique. Total requests number calculated as:
- *
- * total_count = requests_per_second * calling_thread_count
- *
- * By default, SDK initialized in-memory cache with 1 MB capacity.
- * To run the test, you need to start a local OLP mock server first.
- * Valgrind, heaptrack, other tools are used to collect the output.
- */
-TEST_P(MemoryTest, ReadNPartitionsFromVersionedLayer) {
+TEST_P(PrefetchTest, PrefetchPartitionsFromVersionedLayer) {
   const auto& parameter = GetParam();
 
   auto settings = CreateCatalogClientSettings();
 
-  StartThreads([=](uint8_t /*thread_id*/) {
+  StartThreads([=](uint8_t thread_id) {
     olp::dataservice::read::VersionedLayerClient service_client(
         kCatalog, kVersionedLayerId, boost::none, settings);
 
-    const auto end_timestamp =
-        std::chrono::steady_clock::now() + parameter.runtime;
+    const auto level = 10;
 
-    while (end_timestamp > std::chrono::steady_clock::now()) {
-      const auto partition_id = request_counter_.fetch_add(1);
-
-      auto request = olp::dataservice::read::DataRequest().WithPartitionId(
-          std::to_string(partition_id));
-      total_requests_.fetch_add(1);
-      auto token = service_client.GetData(
-          request, [&](olp::dataservice::read::DataResponse response) {
-            if (response.IsSuccessful()) {
-              success_responses_.fetch_add(1);
-            } else {
-              failed_responses_.fetch_add(1);
-              ReportError(response.GetError());
-            }
-          });
-
-      RandomlyCancel(std::move(token));
-
-      std::this_thread::sleep_for(
-          GetSleepPeriod(parameter.requests_per_second));
+    // Generate N tiles with diagonal col/rows (unique for each thread id)
+    std::vector<olp::geo::TileKey> tile_keys(parameter.number_of_tiles);
+    const auto offset = thread_id * parameter.number_of_tiles;
+    for (auto index = 0; index < parameter.number_of_tiles; ++index) {
+      tile_keys[index] =
+          olp::geo::TileKey::FromRowColumnLevel(offset + index, offset, level);
     }
-  });
-}
 
-TEST_P(MemoryTest, PrefetchPartitionsFromVersionedLayer) {
-  const auto& parameter = GetParam();
+    auto request = olp::dataservice::read::PrefetchTilesRequest()
+                       .WithMaxLevel(level + 4)
+                       .WithMinLevel(level)
+                       .WithTileKeys(tile_keys);
 
-  auto settings = CreateCatalogClientSettings();
+    total_requests_.fetch_add(1);
+    auto result = service_client.PrefetchTiles(std::move(request));
+    auto response = result.GetFuture().get();
 
-  StartThreads([=](uint8_t /*thread_id*/) {
-    olp::dataservice::read::VersionedLayerClient service_client(
-        kCatalog, kVersionedLayerId, boost::none, settings);
-
-    const auto end_timestamp =
-        std::chrono::steady_clock::now() + parameter.runtime;
-
-    while (end_timestamp > std::chrono::steady_clock::now()) {
-      const auto level = 10;
-      const auto tile_count = 1 << level;
-
-      std::vector<olp::geo::TileKey> tile_keys = {
-          olp::geo::TileKey::FromRowColumnLevel(rand() % tile_count,
-                                                rand() % tile_count, level)};
-
-      auto request = olp::dataservice::read::PrefetchTilesRequest()
-                         .WithMaxLevel(level + 2)
-                         .WithMinLevel(level)
-                         .WithTileKeys(tile_keys);
-      total_requests_.fetch_add(1);
-      auto token = service_client.PrefetchTiles(
-          std::move(request),
-          [&](olp::dataservice::read::PrefetchTilesResponse response) {
-            if (response.IsSuccessful()) {
-              success_responses_.fetch_add(1);
-            } else {
-              failed_responses_.fetch_add(1);
-              ReportError(response.GetError());
-            }
-          });
-
-      RandomlyCancel(std::move(token));
-
-      std::this_thread::sleep_for(
-          GetSleepPeriod(parameter.requests_per_second));
+    if (response.IsSuccessful()) {
+      success_responses_.fetch_add(1);
+    } else {
+      failed_responses_.fetch_add(1);
+      ReportError(response.GetError());
     }
   });
 }
 
 /*
- * 10 hours stability test with default constructed cache.
- */
-TestConfiguration LongRunningTest() {
-  TestConfiguration configuration;
-  SetErrorFlags(configuration);
-  SetDiskCacheConfiguration(configuration);
-  configuration.configuration_name = "10h_test";
-  configuration.runtime = std::chrono::hours(10);
-  configuration.cancelation_chance = 0.25f;
-  return configuration;
-}
-
-/*
- * Short 5 minutes test to collect SDK allocations without cache.
+ * Test to collect SDK allocations without cache.
  */
 TestConfiguration ShortRunningTestWithNullCache() {
   TestConfiguration configuration;
@@ -262,7 +170,7 @@ TestConfiguration ShortRunningTestWithNullCache() {
 }
 
 /*
- * Short 5 minutes test to collect SDK allocations with in memory cache.
+ * Test to collect SDK allocations with in memory cache.
  */
 TestConfiguration ShortRunningTestWithMemoryCache() {
   TestConfiguration configuration;
@@ -272,8 +180,7 @@ TestConfiguration ShortRunningTestWithMemoryCache() {
 }
 
 /*
- * Short 5 minutes test to collect SDK allocations with both in memory cache
- * and disk cache.
+ * Test to collect SDK allocations with both in memory cache and disk cache.
  */
 TestConfiguration ShortRunningTestWithMutableCache() {
   TestConfiguration configuration;
@@ -287,7 +194,6 @@ std::vector<TestConfiguration> Configurations() {
   configurations.emplace_back(ShortRunningTestWithNullCache());
   configurations.emplace_back(ShortRunningTestWithMemoryCache());
   configurations.emplace_back(ShortRunningTestWithMutableCache());
-  configurations.emplace_back(LongRunningTest());
   return configurations;
 }
 
@@ -295,6 +201,6 @@ std::string TestName(const testing::TestParamInfo<TestConfiguration>& info) {
   return info.param.configuration_name;
 }
 
-INSTANTIATE_TEST_SUITE_P(MemoryUsage, MemoryTest,
+INSTANTIATE_TEST_SUITE_P(MemoryUsage, PrefetchTest,
                          ::testing::ValuesIn(Configurations()), TestName);
 }  // namespace
