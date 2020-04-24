7b6ecb9f4bf4caf5b021c40d8530b56c63ea8929
Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>
2020-04-24 14:13:01 +0300

    Improvement for FV scripts (#802)
    During development we found that .variables injection may be
    failed and skipped as error during FV execution. It's caused
    by exit status which we set after FV functinal tests passed.
    To fix that I moved variables injection on higher level.
    Do the same for all FV test group.This way is already used
    in NV tests.
    
    Relates-To: OLPEDGE-1738
    
    Signed-off-by: Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>

--- scripts/linux/fv/gitlab-olp-cpp-sdk-core-test.sh
+++ scripts/linux/fv/gitlab-olp-cpp-sdk-core-test.sh
@@ -19,7 +19,6 @@
 
 
 echo ">>> Core Test ... >>>"
-source $FV_HOME/olp-cpp-sdk-core-test.variables
 $REPO_HOME/build/olp-cpp-sdk-core/tests/olp-cpp-sdk-core-tests \
     --gtest_output="xml:$REPO_HOME/reports/olp-core-test-report.xml" \
     --gtest_filter=-"DefaultCacheTest.BadPathMutable"
--- scripts/linux/fv/gitlab-olp-cpp-sdk-dataservice-read-test.sh
+++ scripts/linux/fv/gitlab-olp-cpp-sdk-dataservice-read-test.sh
@@ -19,6 +19,5 @@
 
 
 echo ">>> Core - SOURCE DATASERVICE READ Test ... >>>"
-source $FV_HOME/olp-cpp-sdk-dataservice-read-test.variables
 $REPO_HOME/build/olp-cpp-sdk-dataservice-read/tests/olp-cpp-sdk-dataservice-read-tests \
     --gtest_output="xml:$REPO_HOME/reports/olp-dataservice-read-test-report.xml"
