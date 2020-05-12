f870eeb7f54dfc740b9cee81f6659569cc91c167
Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>
2020-05-12 14:38:50 +0300

    Fix the cppcheck paramaters. (#831)
    Resolves: OLPEDGE-1923
    
    Signed-off-by: Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>

--- scripts/linux/nv/gitlab_cppcheck_and_upload_sonar.sh
+++ scripts/linux/nv/gitlab_cppcheck_and_upload_sonar.sh
@@ -26,31 +26,17 @@ export NV_HOME=${REPO_HOME}/scripts/linux/nv
 cppcheck_folder="${REPO_HOME}/reports/cppcheck"
 mkdir -p "$cppcheck_folder"
 
-cppcheck --xml --xml-version=2 --enable=all --inconclusive -I olp-cpp-sdk-authentication/include/olp/authentication/ olp-cpp-sdk-authentication/src 2> cppcheck.xml
-cp cppcheck.xml "$cppcheck_folder/olp-cpp-sdk-authentication-cppcheck.xml"
-
-cppcheck --xml --xml-version=2 --enable=all --inconclusive -I olp-cpp-sdk-dataservice-read/include/olp/dataservice/read/ \
--I olp-cpp-sdk-dataservice-read/include/olp/dataservice/read/model/ olp-cpp-sdk-dataservice-read/src 2>cppcheck.xml
-cp cppcheck.xml "$cppcheck_folder/olp-cpp-sdk-dataservice-read-cppcheck.xml"
-
-cppcheck --xml --xml-version=2 --enable=all --inconclusive -I olp-cpp-sdk-dataservice-write/include/olp/dataservice/write/ \
--I olp-cpp-sdk-dataservice-write/include/olp/dataservice/write/model/  \
--I olp-cpp-sdk-dataservice-write/include/olp/dataservice/write/generated/model/ olp-cpp-sdk-dataservice-write/src 2>cppcheck.xml
-cp cppcheck.xml "$cppcheck_folder/olp-cpp-sdk-dataservice-write-cppcheck.xml"
-
-cppcheck --xml --xml-version=2 --enable=all --inconclusive -I olp-cpp-sdk-core/include/olp/core/client/ \
--I olp-cpp-sdk-core/include/olp/core/context/ \
--I olp-cpp-sdk-core/include/olp/core/cache/ \
--I olp-cpp-sdk-core/include/olp/core/geo/ \
--I olp-cpp-sdk-core/include/olp/core/http/ \
--I olp-cpp-sdk-core/include/olp/core/logging/ \
--I olp-cpp-sdk-core/include/olp/core/math/ \
--I olp-cpp-sdk-core/include/olp/core/generated/parser/ \
--I olp-cpp-sdk-core/include/olp/core/generated/serializer/ \
--I olp-cpp-sdk-core/include/olp/core/porting/ \
--I olp-cpp-sdk-core/include/olp/core/thread/  \
--I olp-cpp-sdk-core/include/olp/core/utils/ olp-cpp-sdk-core/src 2>cppcheck.xml
-cp cppcheck.xml "$cppcheck_folder/olp-cpp-sdk-core-cppcheck.xml"
+cppcheck --xml --xml-version=2 --enable=all --inconclusive \
+    -I olp-cpp-sdk-core/include \
+    -I olp-cpp-sdk-authentication/include \
+    -I olp-cpp-sdk-dataservice-read/include \
+    -I olp-cpp-sdk-dataservice-write/include \
+    olp-cpp-sdk-core/src \
+    olp-cpp-sdk-authentication/src \
+    olp-cpp-sdk-dataservice-read/src \
+    olp-cpp-sdk-dataservice-write/src \
+    2>cppcheck.xml
+mv cppcheck.xml "$cppcheck_folder/cppcheck.xml"
 
 # Set needed properties for Valgrind
 echo "sonar.host.url=${SONAR_HOST}" >> ${NV_HOME}/sonar-project.propertiesValgrind
