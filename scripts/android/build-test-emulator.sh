818d616dfa92d8717a5a2b5a763eef6a9229e781
Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>
2020-05-18 14:43:46 +0300

    Add script for Azure MacOS hosted VM
    Emulator must run in it due to Azure docs.
    
    Relates-To: OLPEDGE-1774
    
    Signed-off-by: Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>

--- scripts/android/build-test-emulator.sh
+++ scripts/android/build-test-emulator.sh
@@ -18,23 +18,42 @@
 # License-Filename: LICENSE
 
 
-mkdir -p build && cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
-	-DANDROID_PLATFORM=android-25 \
-	-DANDROID_ABI=arm64-v8a \
-	-DOLP_SDK_ENABLE_TESTING=NO \
-	-DOLP_SDK_BUILD_EXAMPLES=ON
+#mkdir -p build && cd build
+#cmake .. -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
+#	-DANDROID_PLATFORM=android-25 \
+#	-DANDROID_ABI=arm64-v8a \
+#	-DOLP_SDK_ENABLE_TESTING=NO \
+#	-DOLP_SDK_BUILD_EXAMPLES=ON
 
 #cmake --build . # this is alternative option for build
-sudo make install -j$(nproc)
-cd -
+#sudo make install -j$(nproc)
+#cd -
 
 ls -la $ANDROID_HOME
 export PATH=$PATH:$ANDROID_HOME/tools/bin/
 sdkmanager --list
-sdkmanager "platform-tools" "platforms;android-25" "emulator"
-sdkmanager "system-images;android-25;google_apis;arm64-v8a"
-echo no | avdmanager create avd -n android-25 -k "system-images;android-25;google_apis;arm64-v8a"
-emulator -avd android-25 -no-snapshot -noaudio -no-boot-anim -gpu off -no-accel -no-window -camera-back none -camera-front none -selinux permissive -qemu -m 2048 &
-./scripts/android/android-wait-for-emulator.sh
-adb shell input keyevent 82 &
+
+# Install AVD files
+echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-27;google_apis;x86'
+
+# Create emulator
+echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n xamarin_android_emulator -k 'system-images;android-27;google_apis;x86' --force
+
+$ANDROID_HOME/emulator/emulator -list-avds
+
+echo "Starting emulator"
+
+# Start emulator in background
+nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot > /dev/null 2>&1 &
+$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+
+$ANDROID_HOME/platform-tools/adb devices
+
+echo "Emulator started"
+
+#sdkmanager "platform-tools" "platforms;android-25" "emulator"
+#sdkmanager "system-images;android-25;google_apis;arm64-v8a"
+#echo no | avdmanager create avd -n android-25 -k "system-images;android-25;google_apis;arm64-v8a"
+#emulator -avd android-25 -no-snapshot -noaudio -no-boot-anim -gpu off -no-accel -no-window -camera-back none -camera-front none -selinux permissive -qemu -m 2048 &
+#./scripts/android/android-wait-for-emulator.sh
+#adb shell input keyevent 82 &
