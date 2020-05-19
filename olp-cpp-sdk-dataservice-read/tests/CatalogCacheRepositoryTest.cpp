02c98a6768518854b8d3bad2d08522d958c23255
Kostiantyn Zvieriev <ext-kostiantyn.zvieriev@here.com>
2020-05-19 14:39:57 +0300

    Default expiration in CatalogCacheRepository (#849)
    Default cache items expiration time can be controlled through
    OlpClientOptions, so CatalogCacheRepository should use it instead of
    maximum time_t value. Added unit tests.
    
    Resolves: OLPEDGE-1917
    
    Signed-off-by: Kostiantyn Zvieriev <ext-kostiantyn.zvieriev@here.com>

---
+++ olp-cpp-sdk-dataservice-read/tests/CatalogCacheRepositoryTest.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2020 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+#include "repositories/CatalogCacheRepository.h"
+
+#include <gmock/gmock.h>
+#include <olp/core/cache/CacheSettings.h>
+#include <olp/core/cache/KeyValueCache.h>
+#include <olp/core/client/OlpClientSettingsFactory.h>
+
+namespace {
+
+using namespace olp;
+using namespace olp::dataservice::read;
+
+constexpr auto kCatalog = "hrn:here:data::olp-here-test:catalog";
+
+TEST(PartitionsCacheRepositoryTest, DefaultExpiry) {
+  const auto hrn = client::HRN::FromString(kCatalog);
+
+  model::Catalog model_catalog;
+
+  {
+    SCOPED_TRACE("Disable expiration");
+
+    const auto default_expiry = std::chrono::seconds::max();
+    std::shared_ptr<cache::KeyValueCache> cache =
+        olp::client::OlpClientSettingsFactory::CreateDefaultCache({});
+    repository::CatalogCacheRepository repository(hrn, cache, default_expiry);
+
+    repository.Put(model_catalog);
+    const auto result = repository.Get();
+
+    EXPECT_TRUE(result);
+  }
+
+  {
+    SCOPED_TRACE("Expired");
+
+    const auto default_expiry = std::chrono::seconds(-1);
+    std::shared_ptr<cache::KeyValueCache> cache =
+        olp::client::OlpClientSettingsFactory::CreateDefaultCache({});
+    repository::CatalogCacheRepository repository(hrn, cache, default_expiry);
+
+    repository.Put(model_catalog);
+    const auto result = repository.Get();
+
+    EXPECT_FALSE(result);
+  }
+}
+
+}  // namespace
