348e1bb664a8f03292603e0780c6b5cad5091f1d
Kostiantyn Zvieriev <ext-kostiantyn.zvieriev@here.com>
2020-07-31 17:11:59 +0300

    Make integration and funcitonal tests async.
    Clients API designed to be used async, so tests should use same
    approach too. Adding task scheduler to such tests will do the job.
    
    Resolves: OLPEDGE-2176
    
    Signed-off-by: Kostiantyn Zvieriev <ext-kostiantyn.zvieriev@here.com>

--- tests/functional/olp-cpp-sdk-dataservice-read/DataserviceReadVersionedLayerClientTest.cpp
+++ tests/functional/olp-cpp-sdk-dataservice-read/DataserviceReadVersionedLayerClientTest.cpp
@@ -218,14 +218,17 @@ TEST_F(DataserviceReadVersionedLayerClientTest, PrefetchWideRange) {
 
   {
     SCOPED_TRACE("Read cached data from the same partition");
-    dataservice_read::DataResponse response;
-    auto token = client->GetData(
-        olp::dataservice::read::TileRequest()
-            .WithTileKey(olp::geo::TileKey::FromHereTile(kTileId))
-            .WithFetchOption(dataservice_read::CacheOnly),
-        [&response](dataservice_read::DataResponse resp) {
-          response = std::move(resp);
-        });
+    std::promise<dataservice_read::DataResponse> promise;
+    auto future = promise.get_future();
+    client->GetData(olp::dataservice::read::TileRequest()
+                        .WithTileKey(olp::geo::TileKey::FromHereTile(kTileId))
+                        .WithFetchOption(dataservice_read::CacheOnly),
+                    [&promise](dataservice_read::DataResponse resp) {
+                      promise.set_value(std::move(resp));
+                    });
+
+    auto response = future.get();
+
     EXPECT_SUCCESS(response);
     ASSERT_TRUE(response.GetResult() != nullptr);
     ASSERT_NE(response.GetResult()->size(), 0u);
