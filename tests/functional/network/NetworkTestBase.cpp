7dccc646203f0df69ff4a32c400e016f089b6a55
Kostiantyn Zvieriev <ext-kostiantyn.zvieriev@here.com>
2020-08-12 10:41:23 +0300

    Setup for network fv tests. (#1015)
    We are going to add bunch of functional network tests. Mock server
    will help to simulate corner test cases. Added script will be used to
    launch test and server separately from other fv tests.
    
    Resolves: OLPEDGE-2211
    
    Signed-off-by: Kostiantyn Zvieriev <ext-kostiantyn.zvieriev@here.com>

---
+++ tests/functional/network/NetworkTestBase.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2020 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+#include "NetworkTestBase.h"
+
+#include <memory>
+
+#include <gtest/gtest.h>
+#include <olp/core/client/OlpClientSettingsFactory.h>
+#include <olp/core/http/Network.h>
+#include <olp/core/http/NetworkSettings.h>
+
+void NetworkTestBase::SetUp() {
+  auto proxy_settings =
+      olp::http::NetworkProxySettings()
+          .WithHostname("localhost")
+          .WithPort(1080)
+          .WithUsername("test_user")
+          .WithPassword("test_password")
+          .WithType(olp::http::NetworkProxySettings::Type::HTTP);
+
+  settings_ = olp::http::NetworkSettings().WithProxySettings(proxy_settings);
+  network_ = olp::client::OlpClientSettingsFactory::
+      CreateDefaultNetworkRequestHandler();
+}
