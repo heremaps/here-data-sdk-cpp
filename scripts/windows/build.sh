16279842dd1222e9d48c48e445fc78bbaa2effce
Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>
2022-01-19 12:11:43 +0200

    [Azure]Replace win-2016 to 2019
    Also replace "Visual Studio 15 2017" to
    "Visual Studio 16 2019".
    Due to Microsoft deprecation notice.
    
    Relates-To: OLPEDGE-2728
    
    Signed-off-by: Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>

--- scripts/windows/build.sh
+++ scripts/windows/build.sh
@@ -20,6 +20,6 @@
 
 [[ -d "build" ]] && rm -rf build
 mkdir build && cd build
-cmake .. -G "Visual Studio 15 2017 Win64" \
+cmake .. -G "Visual Studio 16 2019" -A x64 \
         -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 cmake --build . --config $BUILD_TYPE
