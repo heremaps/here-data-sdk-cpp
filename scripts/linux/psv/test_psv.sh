a57f9b4c4e12f282ec98e61dc40c245b202d0e24
Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>
2023-03-24 13:15:44 +0200

    Fix CodeCov job (#1386)
    Corrected BUILD_TYPE to COVERAGE.
    Add extra ubuntu 18.04 job which now passes Codecov.
    Previously, switch to 20.04 caused failure on Codecov.
    Also add extra 22.04 job on gcc-9.
    Fix test to adapt to gcc-9.
    
    Relates-To: OLPEDGE-2799
    
    Signed-off-by: Yaroslav Stefinko <ext-yaroslav.stefinko@here.com>

--- scripts/linux/psv/test_psv.sh
+++ scripts/linux/psv/test_psv.sh
@@ -41,3 +41,17 @@ $CPP_TEST_SOURCE_DARASERVICE_WRITE/olp-cpp-sdk-dataservice-write-tests \
 echo ">>> Integration Test ... >>>"
 $CPP_TEST_SOURCE_INTEGRATION/olp-cpp-sdk-integration-tests \
     --gtest_output="xml:olp-cpp-sdk-integration-tests-report.xml"
+
+# CodeCov
+# Find all .cpp.o and run gcov for them
+pip install coveragepy
+
+curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import # One-time step
+curl -Os https://uploader.codecov.io/latest/linux/codecov
+curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
+curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
+
+gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
+shasum -a 256 -c codecov.SHA256SUM
+chmod +x codecov
+./codecov -g -v -t ${CODECOV_TOKEN}
