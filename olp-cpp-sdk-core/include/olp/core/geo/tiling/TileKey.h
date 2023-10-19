cbe7725f95e463d904264f2be90343270a48a2d2
Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>
2023-10-19 15:41:44 +0300

    Adapt the code to GCC-13 (#1441)
    cstdint header is required for std::int64_t type
    
    Relates-To: OLPEDGE-2843
    
    Signed-off-by: Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>

--- olp-cpp-sdk-core/include/olp/core/geo/tiling/TileKey.h
+++ olp-cpp-sdk-core/include/olp/core/geo/tiling/TileKey.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2020 HERE Europe B.V.
+ * Copyright (C) 2019-2023 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@
 #pragma once
 
 #include <bitset>
+#include <cstdint>
 #include <string>
 
 #include <boost/optional.hpp>
