0453431e03ee1f1149e0793a9cd208e40b847c44
Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>
2023-10-19 13:49:00 +0300

    Adapt the code to GCC-13
    cstdint header is required for std::int64_t type
    
    Relates-To: OLPEDGE-2843
    Signed-off-by: Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>

--- olp-cpp-sdk-authentication/src/Crypto.cpp
+++ olp-cpp-sdk-authentication/src/Crypto.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2020 HERE Europe B.V.
+ * Copyright (C) 2019-2023 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@
 #include <olp/authentication/Crypto.h>
 
 #include <algorithm>
+#include <cstdint>
 
 namespace olp {
 namespace authentication {
--- olp-cpp-sdk-core/src/cache/ProtectedKeyList.h
+++ olp-cpp-sdk-core/src/cache/ProtectedKeyList.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 HERE Europe B.V.
+ * Copyright (C) 2020-2023 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@
 
 #pragma once
 
+#include <cstdint>
 #include <set>
 #include <string>
 
