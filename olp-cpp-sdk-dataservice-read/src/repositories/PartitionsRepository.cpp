80cd90b5a772d49ede5d6c5f67108d54c345b1db
Rustam Gamidov <108531570+rustam-gamidov-here@users.noreply.github.com>
2024-01-09 13:12:16 +0200

    Allow to cancel partitions stream on parsing (#1462)
    PartitionsSaxHandler::Abort() was useless. And it is not enough to
    Abort() when we are waiting for characters in the stream.
    
    Relates-To: OLPEDGE-2859
    
    Signed-off-by: Rustam Gamidov <ext-rustam.gamidov@here.com>

--- olp-cpp-sdk-dataservice-read/src/repositories/PartitionsRepository.cpp
+++ olp-cpp-sdk-dataservice-read/src/repositories/PartitionsRepository.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2023 HERE Europe B.V.
+ * Copyright (C) 2019-2024 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -633,8 +633,10 @@ client::ApiNoResponse PartitionsRepository::ParsePartitionsStream(
     auto partitions_handler =
         std::make_shared<repository::PartitionsSaxHandler>(partition_callback);
 
-    auto reader_cancellation_token =
-        client::CancellationToken([=]() { partitions_handler->Abort(); });
+    auto reader_cancellation_token = client::CancellationToken([=]() {
+      partitions_handler->Abort();
+      async_stream->CloseStream(client::ApiError::Cancelled());
+    });
 
     if (!context.ExecuteOrCancelled(
             [=]() { return reader_cancellation_token; })) {
