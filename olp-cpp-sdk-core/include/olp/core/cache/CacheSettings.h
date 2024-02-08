77a27776fd8b67bd4577ceb3788a984a0a3ee356
Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>
2024-02-08 12:12:06 +0100

    Fix the GCC13 compilation warning
    Add a missing header
    
    Relates-To: OLPEDGE-2863
    Signed-off-by: Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>

--- olp-cpp-sdk-core/include/olp/core/cache/CacheSettings.h
+++ olp-cpp-sdk-core/include/olp/core/cache/CacheSettings.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2021 HERE Europe B.V.
+ * Copyright (C) 2019-2024 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,10 +19,10 @@
 
 #pragma once
 
+#include <cstdint>
 #include <string>
 
 #include <olp/core/Config.h>
-
 #include <olp/core/CoreApi.h>
 
 #include <olp/core/porting/deprecated.h>
