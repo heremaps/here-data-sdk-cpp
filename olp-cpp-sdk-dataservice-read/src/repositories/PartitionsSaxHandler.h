60d94549e7f6259552eb94c30109ee46c4143db1
Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>
2025-01-03 16:20:51 +0100

    Enable SAX parser to handle compressedDataSize (#1576)
    Parser was not expecting compressedDataSize field and failed to parse
    the JSON.
    
    Relates-To: OCMAM-223
    
    Signed-off-by: Mykhailo Kuchma <ext-mykhailo.kuchma@here.com>

--- olp-cpp-sdk-dataservice-read/src/repositories/PartitionsSaxHandler.h
+++ olp-cpp-sdk-dataservice-read/src/repositories/PartitionsSaxHandler.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 HERE Europe B.V.
+ * Copyright (C) 2023-2025 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -70,6 +70,7 @@ class PartitionsSaxHandler
     kParsingDataHandle,
     kParsingChecksum,
     kParsingDataSize,
+    kParsingCompressedDataSize,
     kParsingCrc,
     kParsingIgnoreAttribute,
 
