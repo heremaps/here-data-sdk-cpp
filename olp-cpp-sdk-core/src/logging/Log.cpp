403add70ab1a3fa672d6569d7bbd84daf3453123
Rustam Gamidov <108531570+rustam-gamidov-here@users.noreply.github.com>
2025-11-06 17:21:14 +0200

    Include missing header used in censoring (#1642)
    Censoring has been added recently but with missing header
    
    Relates-To: NLAM-140
    
    Signed-off-by: Rustam Gamidov <ext-rustam.gamidov@here.com>

--- olp-cpp-sdk-core/src/logging/Log.cpp
+++ olp-cpp-sdk-core/src/logging/Log.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2021 HERE Europe B.V.
+ * Copyright (C) 2019-2025 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@
 #include <olp/core/thread/Atomic.h>
 
 #include <algorithm>
+#include <cstring>
 #include <string>
 #include <unordered_map>
 
